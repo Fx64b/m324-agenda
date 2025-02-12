cc6a9cc7def765cbedc703f2bc38c11af5b78c3c
Fx64b <90456762+Fx64b@users.noreply.github.com>
2025-02-12 12:12:22 +0100

    build(deps): install and configure vitest for testing

---
+++ test.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="vitest" />
+/// <reference types="@testing-library/jest-dom" />
+
+import '@testing-library/jest-dom'
+
+declare module 'vitest' {
+    interface JestAssertion<T = any> extends jest.Matchers<void, T> {}
+}
